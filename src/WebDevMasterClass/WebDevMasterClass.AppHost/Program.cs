01aa41d8393a15b8098cccd9ac4a45ac148d0464
Chris Klug <chris@59north.com>
2024-10-02 09:39:51 +0200

    Fixed UI path

--- src/WebDevMasterClass/WebDevMasterClass.AppHost/Program.cs
+++ src/WebDevMasterClass/WebDevMasterClass.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var ui = builder.AddDockerfile("ui", "../../../_resources/ui")
+var ui = builder.AddDockerfile("ui", "../../../_resources/ui-svelte")
                 .WithHttpEndpoint(targetPort: 80, env: "PORT");
 
 builder.Build().Run();
