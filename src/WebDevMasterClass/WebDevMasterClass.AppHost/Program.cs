a22326d4043ba7523dade9fa4fdaea28ba088456
Chris Klug <chris@59north.com>
2024-10-02 09:51:47 +0200

    Fixed merge issue

--- src/WebDevMasterClass/WebDevMasterClass.AppHost/Program.cs
+++ src/WebDevMasterClass/WebDevMasterClass.AppHost/Program.cs
@@ -5,7 +5,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var ui = builder.AddDockerfile("ui", "../../../_resources/ui")
                 .WithHttpEndpoint(targetPort: 80, env: "PORT");
 
-builder.AddProject<Projects.WebDevMasterClass_Services_Products>("products")
+var products = builder.AddProject<Projects.WebDevMasterClass_Services_Products>("products")
         .WithEnvironment("ConnectionStrings__Sql", builder.Configuration.GetConnectionString("Products"));
 
 builder.AddProject<Projects.WebDevMasterClass_Web>("web", "aspire")
